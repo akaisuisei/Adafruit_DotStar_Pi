f24d158e4b8d262ced4fd7f7033e859b4646c68b
Bjay Watanabe Kamwa <bjay.kamwa-watanabe@snips.ai>
2018-07-24 16:45:56 +0000

    add file for snips-skill-server

--- action-matrix.py
+++ action-matrix.py
@@ -8,11 +8,16 @@ if __name__ == "__main__":
     def sig_handler(sig, frame):
         c.disconnect()
         m.stop()
-        r.stop()
+        if r:
+            r.stop()
         sys.exit(0)
     MQTT_IP_ADDR = "raspi-mika.local"
     signal.signal(signal.SIGINT, sig_handler)
     c = Concierge(MQTT_IP_ADDR, "default", False)
     m = ConciergeMatrix("default", c)
-    r = Remote("default", c)
+    r = None
+    try:
+        r = Remote("default", c)
+    except:
+        pass
     c.loop_forever()
