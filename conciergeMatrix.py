503d8d3732448772cedcbad4547d86a7157aa88e
Bjay Watanabe Kamwa <bjay.kamwa-watanabe@snips.ai>
2018-07-24 09:23:54 +0000

    Change file to class

---
+++ conciergeMatrix.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python2
+# -*-: coding utf-8 -*-
+
+import calendar
+import json
+import os
+import time
+import threading
+from datetime import datetime
+from snipsmatrix import SnipsMatrix
+import socket
+import signal
+import sys
+from concierge_python.concierge import Concierge
+
+class ConciergeMatrix():
+    _id= "snips-skill-matrix"
+    dial_open = 'hermes/asr/startListening'
+    dial_close = 'hermes/asr/stopListening'
+    rotate_count = {'music': 0, 'light':0}
+
+    def __init__(self, siteId, c):
+        self.json_dir = ConciergeMatrix.load_json_dir()
+        self.skill = SnipsMatrix()
+        self.current_session = None
+        self.swipe_num = 0
+        self.active_app = 'music'
+        self.site_id = siteId
+        alive = 0;
+        c.subscribePing(self.on_ping)
+        c.subscribe(ConciergeMatrix.dial_open, self.on_dialogue_open)
+        c.subscribe(ConciergeMatrix.dial_close, self.on_dialogue_close)
+        c.subscribeTime(self.on_time)
+        c.subscribeTimer(self.on_timer)
+        c.subscribeAnimation(self.on_animation)
+        c.subscribeStop(self.on_stop)
+        c.subscribeImage(self.on_image)
+        c.subscribeRotary(self.on_rotary)
+        c.subscribeSwipe(self.on_swipe)
+        c.subscribeWeather(self.on_weather)
+        self.c = c
+
+    @staticmethod
+    def load_json_dir():
+        def load_json(path):
+            data = None
+            with open(path, 'r') as f:
+                data = json.load(f)
+            data['siteId'] = 'default'
+            data['customData'] = None
+            data['sessionId'] = '317r637fhnfcl3u2ej9ienj'
+            return json.dumps(data)
+        data = {}
+        data['inc_light'] = ['hermes/intent/lightsShift', load_json('inc_light.json')]
+        data['dec_light'] = ['hermes/intent/lightsShift', load_json('dec_light.json')]
+        data['inc_music'] = ['hermes/intent/VolumeUp', load_json('inc_music.json')]
+        data['dec_music'] = ['hermes/intent/VolumeDown', load_json('dec_music.json')]
+        return data
+
+    def on_dialogue_open(self, client, userdata, msg):
+        print(msg.topic)
+        data = json.loads(msg.payload)
+        if data['siteId'] == self.site_id:
+            self.skill.hotword_detected()
+            self.current_session = data['sessionId']
+
+    def on_dialogue_close(self, client, userdata, msg):
+        print(msg.topic)
+        data = json.loads(msg.payload)
+        if (data['siteId'] != self.site_id):
+            return
+        if (self.current_session and
+            data['sessionId'] and
+            data['sessionId'] == self.current_session):
+            self.skill.stop_hotword()
+
+    def on_stop(self, client, userdata, msg):
+        print(msg.topic)
+        self.skill.stop()
+
+    def on_image(self, client, userdata, msg):
+        print(msg.topic)
+        tmp = msg.topic.split('/')
+        name = tmp [-1]
+        directory = tmp[-2]
+        if (tmp[-3] != 'add'):
+            return
+        image = msg.payload
+        self.skill.save_image(name, directory, image)
+
+    def on_time(self, client, userdata, msg):
+        print('time')
+        try:
+            data = json.loads(msg.payload)
+        except:
+            print('time not a json')
+            return
+        duration = None
+        if ('duration' in data):
+            duration = data['duration']
+        self.skill.show_time(duration)
+
+    def on_animation(self, client, userdata, msg):
+        print(msg.topic)
+        try:
+            data = json.loads(msg.payload)
+        except:
+            print('animation not a json')
+            return
+        if ('animation' not in data):
+            return
+        animation = data['animation']
+        duration = None
+        if ('duration' in data):
+            duration = data['duration']
+        self.skill.show_animation(animation, duration)
+
+    def on_timer(self, client, userdata, msg):
+        print(msg.topic)
+        try:
+            data = json.loads(msg.payload)
+        except:
+            print('timer not a json')
+            return
+        duration = None
+        if (not isinstance(data, dict)):
+            return
+        if ('duration' in data):
+            duration = data['duration']
+        self.skill.show_timer(duration)
+
+    def on_rotary(self, client, userdata, msg):
+        print(msg.topic)
+        #simulate concierge
+        rotate_count[self.active_app] += int(msg.payload)
+        volume = rotate_count[self.active_app]
+        tmp = "inc_"
+        if (int(msg.payload) <= 0):
+            tmp = 'dec_'
+        data = self.json_dir[tmp + self.active_app]
+        print(data)
+        self.c.publish(data[0], data[1])
+        self.skill.show_rotate(volume)
+
+    def on_swipe(self, client, userdata, msg):
+        apps = ['music', 'light']
+        if msg.payload == 'right' or msg.payload == 'left':
+            self.swipe_num += 1
+        else:
+            self.swipe_num -= 1
+        if swipe_num < 0:
+            self.swipe_num = len(apps) - 1
+        if swipe_num >= len(apps):
+            self.swipe_num = 0
+        print(msg.topic)
+        self.active_app = apps[swipe_num]
+        self.skill.show_animation(apps[swipe_num], None)
+
+    def on_weather(self, client, userdata, msg):
+        try:
+            tmp = json.loads(msg.payload)
+        except:
+            print('weather not a json')
+            return
+        if ('temp' not in tmp or 'weather' not in tmp):
+            return
+        self.skill.show_weather(tmp['temp'], tmp['weather'])
+
+    def on_ping(self, client, userdata, msg):
+        self.c.publishPong(_id)
+
+    def stop(self):
+        self.skill.exit()
+
+if __name__ == "__main__":
+    def sig_handler(sig, frame):
+        c.disconnect()
+        a.stop()
+        sys.exit(0)
+    MQTT_IP_ADDR = "raspi-mika.local"
+    signal.signal(signal.SIGINT, sig_handler)
+    c = Concierge(MQTT_IP_ADDR, "default", False)
+    a = ConciergeMatrix("default", c)
+    c.loop_forever()
---
+++ remote.py
@@ -0,0 +1,144 @@
+from concierge_python.concierge import Concierge
+import RPi.GPIO as GPIO
+from rotary import RotaryEncoder
+from mpu import Mpu
+from apds import Apds
+import json
+import signal
+import sys
+import time
+
+class Remote():
+    MOVE_COUNTER = 5
+    ROTARY_COUNTER = 4
+    asrStart = "hermes/asr/startListening"
+    asrStop = "hermes/asr/stopListening"
+
+    def _init_rotary(self):
+        self.rotary = RotaryEncoder()
+        self.rotary.add_rotate_callback(self.on_rotary)
+        self.rotary.add_push_callback(self.on_press)
+        self.rotary.add_release_callback(self.on_release)
+    def _init_apds(self):
+        self.apds = Apds()
+        self.apds.add_dir_callback(self.on_swipe)
+        self.apds.add_light_up_callback(self.on_lup, 1000)
+        self.apds.add_light_down_callback(self.on_ldown, 300)
+        self.apds.add_near_callback(self.on_near, 40)
+        self.apds.add_far_callback(self.on_far, 17)
+        self.apds.start()
+    def _init_mpu(self):
+        self.mpu = Mpu()
+        self.mpu.add_callback(self.on_move)
+        self.mpu.start()
+
+    def __init__(self, siteId, concierge):
+        self.siteId = siteId
+        self.haveBeenMoved = -1
+        self.rotarySet = -1
+        self.hotwordSended = False
+        self.bPressed = False
+        self.sessionId = None
+        self.c = concierge
+        self.c.subscribe(Remote.asrStart, self.on_startListening)
+        self.c.subscribe(Remote.asrStop, self.on_stopListening)
+        self._init_mpu()
+        self._init_apds()
+        self._init_rotary()
+        self.run = True
+
+    def worker(self):
+        while(self.run):
+            if(self.rotarySet > 0):
+                self.rotarySet -= 1
+                self.haveBeenMoved = -1
+            if (self.bPressed or self.hotwordSended):
+                self.haveBeenMoved = -1
+            if (self.haveBeenMoved == 0):
+                self.sendHotword()
+                self.haveBeenMoved = -1
+            if (self.haveBeenMoved > 0):
+                self.haveBeenMoved -= 1
+            time.sleep(0.2)
+
+    def stop(self):
+        self.run = False
+        self.mpu.stop()
+        self.Apds.run =False
+        sys.exit(0)
+
+    def on_startListening(self, client, userdata, message):
+        if not self.hotwordSended:
+            return
+        msg = json.loads(message.payload.decode("utf-8","ignore"))
+        if(msg['siteId'] != self.siteId):
+            return
+        self.sessionId = msg['sessionId']
+
+    def on_stopListening(self, client, userdata, message):
+        if not self.hotwordSended:
+            return
+        msg = json.loads(message.payload.decode("utf-8","ignore"))
+        if(msg['siteId'] != siteId):
+            return
+        self.hotwordSended = False
+        self.sessionId = None
+
+    def sendHotword(self):
+        self.hotwordSended = True
+        self.c.startHotword()
+
+    def sendStopHotword(self):
+        self.hotwordSended = False
+        self.c.stopHotword()
+
+    def on_rotary(self, degree):
+        if (self.bPressed):
+            return
+        self.rotarySet = Remote.ROTARY_COUNTER
+        self.c.publishRotary(degree)
+
+    def on_press(self):
+        print("button press")
+        self.bPressed =True
+        self.sendHotword()
+
+    def on_release(self):
+        if(not self.bPressed or self.sessionId is None):
+            return
+        print("button release")
+        self.sendStopHotword()
+        self.sessionId = None
+        self.bPressed = False
+
+    def on_swipe(self, direction):
+        print("direction: {}".format(direction))
+        self.c.publishSwipe(direction)
+
+    def on_move(self):
+        if (not self.bPressed and self.rotarySet <= 0):
+            self.haveBeenMoved = Remote.MOVE_COUNTER
+            print("haveBeenMoved")
+
+    def on_lup(self):
+        print("lup")
+
+    def on_ldown(self):
+        print("ldown")
+
+    def on_near(self):
+        print("near")
+
+    def on_far(self):
+        print("far")
+
+if __name__ == "__main__":
+    def sig_handler(sig, frame):
+        c.disconnect()
+        a.stop()
+        sys.exit(0)
+    MQTT_IP_ADDR = "raspi-mika.local"
+    signal.signal(signal.SIGINT, sig_handler)
+    c = Concierge(MQTT_IP_ADDR, "default", False)
+    a = Remote("default", c)
+    c.loop_forever()
